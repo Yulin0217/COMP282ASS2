88c6cf88317e2311834b6647041ea8348581278b
Yulin <715054001@qq.com>
2023-05-13 01:56:48 +0100

    0

---
+++ ASS2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DrawLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picLines = new System.Windows.Forms.PictureBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLines)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picLines
+            // 
+            this.picLines.Location = new System.Drawing.Point(83, 64);
+            this.picLines.Name = "picLines";
+            this.picLines.Size = new System.Drawing.Size(372, 342);
+            this.picLines.TabIndex = 0;
+            this.picLines.TabStop = false;
+            this.picLines.Paint += new System.Windows.Forms.PaintEventHandler(this.PicLines_Paint);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(544, 109);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(133, 33);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Make a random line";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.BtnRandom_Click);
+            this.btnRandom.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BtnRandom_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(522, 190);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(372, 342);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(269, 328);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(274, 121);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.picLines);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picLines)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLines;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ASS2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLines
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Timers.Timer t = new System.Timers.Timer();
+            t.Interval = 3000;
+            t.Elapsed += T_Elapsed;
+            t.Start();
+        }
+
+        private void T_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            MessageBox.Show("Times up");
+        }
+
+        private void BtnRandom_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Point first = new Point((int)(picLines.Size.Width * rnd.NextDouble()), (int)(picLines.Size.Height * rnd.NextDouble()));
+            Point second = new Point((int)(picLines.Size.Width * rnd.NextDouble()), (int)(picLines.Size.Height * rnd.NextDouble()));
+            Color c = Color.FromArgb((int)(255 * rnd.NextDouble()), (int)(255 * rnd.NextDouble()),(int)(255 * rnd.NextDouble()));
+            Tuple<Point, Point, Color> l = new Tuple<Point, Point, Color>(first, second, c);
+            lines.Add(l);
+             picLines.Invalidate();
+        }
+
+        List<Tuple<Point, Point, Color>> lines = new List<Tuple<Point, Point, Color>>();
+        private void PicLines_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var l in lines)
+            {
+                Pen p = new Pen(l.Item3);
+                e.Graphics.DrawLine(p, l.Item1, l.Item2);
+            }
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRandom_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
