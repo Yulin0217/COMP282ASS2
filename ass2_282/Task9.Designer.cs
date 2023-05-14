8b4d1b1990a0717693b785d0dbe3a0d7a468bf6b
Yulin <715054001@qq.com>
2023-05-14 06:00:47 +0100

    update for task9

--- ass2_282/Task9.Designer.cs
+++ ass2_282/Task9.Designer.cs
@@ -33,12 +33,145 @@ namespace DrawLines
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new Container();
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(800, 450);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.remove_btn = new System.Windows.Forms.Button();
+            this.color_btn = new System.Windows.Forms.Button();
+            this.find_btn = new System.Windows.Forms.Button();
+            this.picture_box2 = new System.Windows.Forms.PictureBox();
+            this.color_dialog_2 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.remove_btn);
+            this.panel1.Controls.Add(this.color_btn);
+            this.panel1.Controls.Add(this.find_btn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(872, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 774);
+            this.panel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.Column1, this.Column2, this.Column3, this.Column4, this.Column5 });
+            this.dataGridView1.Location = new System.Drawing.Point(6, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(298, 463);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_cell_click);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "First X";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 55;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "First Y";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 55;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Second X";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 60;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Second Y";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 60;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Color";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 60;
+            // 
+            // remove_btn
+            // 
+            this.remove_btn.Location = new System.Drawing.Point(179, 32);
+            this.remove_btn.Name = "remove_btn";
+            this.remove_btn.Size = new System.Drawing.Size(103, 39);
+            this.remove_btn.TabIndex = 15;
+            this.remove_btn.Text = "Remove";
+            this.remove_btn.UseVisualStyleBackColor = true;
+            this.remove_btn.Click += new System.EventHandler(this.remove_btn_Click_1);
+            // 
+            // color_btn
+            // 
+            this.color_btn.Location = new System.Drawing.Point(28, 32);
+            this.color_btn.Name = "color_btn";
+            this.color_btn.Size = new System.Drawing.Size(105, 39);
+            this.color_btn.TabIndex = 14;
+            this.color_btn.Text = "Color";
+            this.color_btn.UseVisualStyleBackColor = true;
+            this.color_btn.Click += new System.EventHandler(this.color_btn_Click);
+            // 
+            // find_btn
+            // 
+            this.find_btn.Location = new System.Drawing.Point(93, 608);
+            this.find_btn.Name = "find_btn";
+            this.find_btn.Size = new System.Drawing.Size(134, 55);
+            this.find_btn.TabIndex = 13;
+            this.find_btn.Text = "Find InterSections";
+            this.find_btn.UseVisualStyleBackColor = true;
+            // 
+            // picture_box2
+            // 
+            this.picture_box2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picture_box2.Location = new System.Drawing.Point(0, 0);
+            this.picture_box2.Name = "picture_box2";
+            this.picture_box2.Size = new System.Drawing.Size(872, 774);
+            this.picture_box2.TabIndex = 4;
+            this.picture_box2.TabStop = false;
+            this.picture_box2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseUp);
+            // 
+            // Task9
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 774);
+            this.Controls.Add(this.picture_box2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Task9";
             this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box2)).EndInit();
+            this.ResumeLayout(false);
         }
 
+        private System.Windows.Forms.Button find_btn;
+        private System.Windows.Forms.Button color_btn;
+        private System.Windows.Forms.Button remove_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Panel panel1;
+
+        private System.Windows.Forms.PictureBox picture_box2;
+        private System.Windows.Forms.ColorDialog color_dialog_2;
         #endregion
     }
 }
--- ass2_282/Task9.cs
+++ ass2_282/Task9.cs
@@ -1,12 +1,188 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace DrawLines
 {
     public partial class Task9 : Form
     {
+        private List<Line> _lines = new List<Line>();
+        private List<Point> _points = new List<Point>();
+
         public Task9()
         {
             InitializeComponent();
+        }
+
+
+        private void color_btn_Click(object sender, EventArgs e)
+        {
+            if (color_dialog_2.ShowDialog() == DialogResult.OK)
+            {
+                color_btn.ForeColor = color_dialog_2.Color;
+            }
+        }
+
+        public void draw_add()
+        {
+            try
+            {
+                remove_btn.Enabled = true;
+                Graphics g = picture_box2.CreateGraphics();
+                g.Clear(picture_box2.BackColor);
+                dataGridView1.Rows.Clear();
+                foreach (Line line in _lines)
+                {
+                    Pen pen = new Pen(line.LineColor);
+                    g.DrawLine(pen, line.Point1, line.Point2);
+                    //Add new line in datagridview
+                    int rowIndex = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rowIndex].Cells[0].Value = line.Point1.X;
+                    dataGridView1.Rows[rowIndex].Cells[1].Value = line.Point1.Y;
+                    dataGridView1.Rows[rowIndex].Cells[2].Value = line.Point2.X;
+                    dataGridView1.Rows[rowIndex].Cells[3].Value = line.Point2.Y;
+                    dataGridView1.Rows[rowIndex].Cells[4].Style.BackColor = line.LineColor;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void DrawLine(Line line)
+        {
+            Graphics g = picture_box2.CreateGraphics();
+            Pen pen = new Pen(color_btn.ForeColor);
+            g.DrawLine(pen, line.Point1, line.Point2);
+        }
+
+        private void DrawLine_for_grid()
+        {
+            Graphics g = picture_box2.CreateGraphics();
+            g.Clear(picture_box2.BackColor);
+            foreach (Line line in _lines)
+            {
+                Pen pen = new Pen(line.LineColor);
+                g.DrawLine(pen, line.Point1, line.Point2);
+            }
+        }
+
+        private void PictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            remove_btn.Enabled = true;
+            if (e.Button == MouseButtons.Left)
+            {
+                //Add location to points container
+                _points.Add(e.Location);
+                if (_points.Count == 2)
+                {
+                    //Use start and end position to make a line
+                    Line line = new Line(_points.ToArray()[0], _points.ToArray()[1], color_btn.ForeColor);
+                    _lines.Add(line);
+                    draw_add();
+                    _points.Clear();
+                }
+            }
+        }
+
+
+        private void remove_btn_Click_1(object sender, EventArgs e)
+        {
+            // Get the currently selected item index
+            DataGridViewCell selected_cell = dataGridView1.CurrentCell;
+            
+            //Check whether there is no line
+            if (_lines.Count == 0)
+            {
+                MessageBox.Show("Nothing selected or select empty cell");
+                return;
+            }
+
+
+            _lines.RemoveAt(selected_cell.RowIndex);
+            draw_add();
+            if (_lines.Count == 0)
+            {
+                // No lines left, disable remove button and clear picture box
+                remove_btn.Enabled = false;
+                Graphics g = picture_box2.CreateGraphics();
+                g.Clear(Color.White);
+            }
+        }
+
+        private void dataGridView1_cell_click(object sender, DataGridViewCellEventArgs e)
+        {
+            //Get current cell index
+            int row_index = e.RowIndex;
+            int column_index = e.ColumnIndex;
+
+            // Check if click color cell (4)
+            if (column_index == 4)
+            {
+                if (color_dialog_2.ShowDialog() == DialogResult.OK)
+                {
+                    var colorSet = color_dialog_2.Color;
+                    _lines[row_index].LineColor = colorSet;
+                    draw_add();
+                }
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewCell cell = dataGridView1.CurrentCell;
+
+                if (cell.Value == null)
+                {
+                    return;
+                }
+
+                var FirstX = dataGridView1.Rows[cell.RowIndex].Cells[0].Value;
+                var FirstY = dataGridView1.Rows[cell.RowIndex].Cells[1].Value;
+                var SecondX = dataGridView1.Rows[cell.RowIndex].Cells[2].Value;
+                var SecondY = dataGridView1.Rows[cell.RowIndex].Cells[3].Value;
+                if (FirstX == null || FirstY == null || SecondX == null || SecondY == null)
+                {
+                    return;
+                }
+
+                //Check if digits using regex
+                string inputFirstx = FirstX.ToString();
+                string inputFirsty = FirstY.ToString();
+                string inputSecondx = SecondX.ToString();
+                string inputSecondy = SecondY.ToString();
+                if (!(Regex.IsMatch(inputFirstx, "^[0-9]+$") || Regex.IsMatch(inputFirsty, "^[0-9]+$") || Regex.IsMatch(
+                        inputSecondx, "^[0-9]+$") || Regex.IsMatch(inputSecondy, "^[0-9]+$")))
+                {
+                    MessageBox.Show("Can't be not digits!!!");
+                    return;
+                }
+
+                _lines.Clear();
+
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    // Get first element in row
+                    int X1 = int.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    int Y1 = int.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                    int X2 = int.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString());
+                    int Y2 = int.Parse(dataGridView1.Rows[i].Cells[3].Value.ToString());
+                    Color colorSet = dataGridView1.Rows[i].Cells[4].Style.BackColor;
+                    Line line = new Line(new Point(X1, Y1), new Point(X2, Y2), colorSet);
+                    _lines.Add(line);
+                }
+
+                DrawLine_for_grid();
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("Please click Color cell again");
+            }
         }
     }
 }
